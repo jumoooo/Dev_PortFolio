2b5681995540570d01643c890085fc9c2dd18dfc
준모 김 <rnsdlsdmltk@gmail.com>
2025-06-24 16:00:12 +0900

    재커밋

--- src/pages/portfolio.vue
+++ src/pages/portfolio.vue
@@ -24,19 +24,39 @@
       <PortfolioContents title="Carrer">
         <template #body
           ><div class="carrers-wrap">
-            <figure class="carrer-figure flex flex-center">
-              <img
-                src="http://www.lisner.co.kr/theme/s007/img/logo-footer.png"
-                alt="리스너 로고"
-              />
-            </figure>
+            <PortfolioFigure
+              :image-link="'http://www.lisner.co.kr/theme/s007/img/logo-footer.png'"
+              :alt="'리스너 로고'"
+            />
           </div>
         </template>
       </PortfolioContents>
     </section>
     <section id="personal-activities">
       <PortfolioContents title="Personal Activities">
-        <template #body> </template>
+        <template #body>
+          <div class="flex-wrap row q-col-gutter-md flex flex-center">
+            <div class="col-4" v-for="n in 6" :key="`sm-${n}`">
+              <div class="flip-container">
+                <div class="flipper">
+                  <div class="flip-box-front">
+                    <img
+                      class="front-img"
+                      src="https://news.nateimg.co.kr/orgImg/ed/2024/10/30/PS24103001222.jpg"
+                    />
+                    <div class="front-title flex">
+                      <p>Poke Card Interaction Page</p>
+                      <strong>개인 프로젝트</strong>
+                    </div>
+                  </div>
+                  <div class="flip-box-back">
+                    <p class="text-h4 text-white">Back</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </template>
       </PortfolioContents>
     </section>
   </section>
@@ -45,9 +65,13 @@
 <script setup>
 import PortfolioHeader from './portfolio/PortfolioHeader.vue';
 import PortfolioContents from './portfolio/PortfolioContents.vue';
+import PortfolioFigure from './portfolio/PortfolioFigure.vue';
 </script>
 
 <style lang="scss" scoped>
+p {
+  color: #444444;
+}
 #containar {
   display: block;
   line-height: 1.8;
@@ -76,9 +100,109 @@ import PortfolioContents from './portfolio/PortfolioContents.vue';
   background-color: #222944;
   height: 100%;
   width: 100%;
+  transition: all 0.2s linear;
+  border-radius: 3px;
 }
-p {
-  color: #444444;
+.carrer-figure:hover {
+  transform: scale(1.1);
+}
+.flex-wrap {
+  width: 100%;
+  padding-bottom: 50px;
+}
+.flip-container {
+  min-height: 300px;
+  perspective: 1000px;
+}
+.flipper {
+  width: 100%;
+  height: 100%;
+  min-height: 300px;
+  border-radius: 3px;
+  box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.3);
+  text-align: center;
+
+  transform-style: preserve-3d;
+  transition: transform 0.5s ease 0.25s, -webkit-transform 0.5s ease 0.25s;
+}
+
+/* 마우스 오버 시 뒤집기 */
+.flip-container:hover .flipper {
+  // 개발 임시 주석중
+  transform: rotateY(-180deg); /* Y축으로 -180도 회전 (우에서 좌로 뒤집기) */
+}
+
+.flip-box-front,
+.flip-box-back {
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 3px;
+
+  position: absolute;
+  -webkit-backface-visibility: hidden; /* 뒷면 요소가 보이지 않도록 숨김 */
+  backface-visibility: hidden; /* 뒷면 요소가 보이지 않도록 숨김 */
+}
+
+.flip-box-front {
+  // background-color: #fff;
+  color: black;
+}
+
+.front-title {
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 0 20px;
+
+  flex-direction: column;
+  z-index: 2;
+
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  background: linear-gradient(180deg, hsla(0, 0%, 100%, 0), #e5e5e5f3);
+
+  strong,
+  p {
+    text-align: left;
+    // line-height: 1.2;
+    font-size: 16px;
+    margin: 0;
+  }
+}
+.front-img {
+  width: auto;
+  max-width: 100%;
+  max-height: 100%;
+  border-radius: 3px;
+}
+
+.flip-box-back {
+  background-color: #222944; /* 뒷면의 배경색 */
+  color: white;
+  transform: rotateY(180deg); /* 뒷면을 미리 뒤집어 놓기 */
+}
+.text-center {
+  text-align: center;
+}
+@media (max-width: 786px) {
+  .front-img {
+    padding-bottom: 40px;
+  }
+  .front-title {
+    justify-content: center !important;
+    strong,
+    p {
+      font-size: 12px !important;
+    }
+  }
 }
 </style>
 
---
+++ src/pages/portfolio/PortfolioFigure.vue
@@ -0,0 +1,39 @@
+<!-- 로고 및 이미지 링크 -->
+<template>
+  <figure class="custom-figure flex flex-center">
+    <img :src="imageLink" :alt="alt" />
+    <figcaption v-if="caption">{{ caption }}</figcaption>
+  </figure>
+</template>
+
+<script setup>
+defineProps({
+  imageLink: {
+    type: String,
+    default: '',
+  },
+  alt: {
+    type: String,
+    default: '',
+  },
+  caption: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.custom-figure {
+  background-color: #222944;
+  height: 100%;
+  width: 100%;
+  transition: all 0.2s linear;
+  border-radius: 3px;
+
+  cursor: pointer;
+}
+.custom-figure:hover {
+  transform: scale(1.1);
+}
+</style>
